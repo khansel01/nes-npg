7dfc184d4d3a43c138ab41f9ceb9bbab2318beee
Unknown <janosch_moos@online.de>
2018-11-22 17:50:13 +0100

    Tuning of Softmax

--- NPG_GaussianPolicy_20.11.18.py
+++ NPG_GaussianPolicy_20.11.18.py
@@ -13,14 +13,14 @@ import matplotlib.pyplot as plt
 # Observations: 1. Cart Position   2. Cart Velocity    3. Pole Angle   4. Pole Velocity at Tip
 # Actions: 1. Left  2. Right
 env = gym.make('CartPole-v0')
-env.seed(1995)
+env.seed(1)
 
 # Define Parameters [W, b, sigma]
 # --------------------------------
 # W consists of 4 weights - one for each observation
 # delta is the normalized step size of the parameter update
-W1 = np.zeros((1, 4))*0.0
-W2 = np.zeros((1, 4))*0.0
+W1 = np.ones((1, 4))*1.0
+W2 = np.ones((1, 4))*-1.0
 delta = 0.05
 
 # Define training setup
@@ -29,10 +29,12 @@ delta = 0.05
 # T is the max number of steps in a single run of the simulation
 # K is the number of iterations for training the algorithm
 T = 200
-K = 1000
+K = 600
 lambda_ = 0.95
 gamma_ = 0.98
 Reward = 0
+max_reward = 0
+eps = np.finfo(np.float32).eps.item()
 
 
 # Choose an action based on higher probability using policy function
@@ -44,19 +46,17 @@ def choose_action(observation):
     p1 = a1 / (a1+a2)
     p2 = a2 / (a1+a2)
     # print("Prob: ", p1, p2)
-    if p1 >= p2:
-        return 1
-    else:
-        return 0
+    return p1, p2
 
 
 def softmax_policy(observation, action, W):
     obs = np.zeros(len(observation))
-    W = np.append(W, 1)
+    # W = np.append(W, 1)
     for i, ele in enumerate(observation):
         obs[i] = ele
-    phi = np.append(obs, action)
-    return np.exp(np.dot(phi, np.transpose(W)))
+    # phi = np.append(obs, action)
+    # print(np.exp(np.dot(obs, np.transpose(W))))
+    return np.sum(np.exp(np.dot(obs, np.transpose(W))))
 
 
 def compute_advantage(transitions):
@@ -64,29 +64,27 @@ def compute_advantage(transitions):
     for i, transition in enumerate(transitions):
         for remainingsteps in range(len(transitions)-i-1):
             advantage[i] += ((gamma_*lambda_)**remainingsteps) * transition[2]
-    advantage = (advantage - np.mean(advantage))/(np.std(advantage) + 0.0001)
+    advantage = (advantage - np.mean(advantage))/(np.std(advantage) + eps)
     return advantage
 
 
 def log_policy_gradient_softmax(transitions):
-    log_p_gradient = np.random.sample((8, len(transitions)))
+    log_p_gradient = np.random.sample((10, len(transitions)))
     for i, transition in enumerate(transitions):
         observation = transition[0]
         action = transition[1]
         obs = np.zeros(len(observation))
         for i, ele in enumerate(observation):
             obs[i] = ele
-        phi = np.transpose(np.append(obs, action))
+        phi = np.transpose(obs)
         a1 = softmax_policy(observation, 1.0, W1)
         a2 = softmax_policy(observation, 0.0, W2)
         p1 = a1 / (a1 + a2)
         p2 = a2 / (a1 + a2)
-        phi1 = np.transpose(np.append(obs, 1.0))
-        phi2 = np.transpose(np.append(obs, 0.0))
-        if action == 1:
-            log_p_gradient[0:4, i] = (phi - phi1*p1 - phi2*p2)[0:4]
-        else:
-            log_p_gradient[4:8, i] = (phi - phi1 * p1 - phi2 * p2)[0:4]
+        phi1 = np.transpose(np.append(obs, 1.0))[0:4]
+        phi2 = np.transpose(np.append(obs, 0.0))[0:4]
+        log_p_gradient[0:4, i] = (phi - phi1*p1 - phi2*p2)
+        log_p_gradient[4:8, i] = (phi - phi1*p1 - phi2*p2)
     return log_p_gradient
 
 
@@ -118,26 +116,28 @@ def update_parameters(log_p_gradient, advantage):
 # Termination conditions are pole angle +/- 12, cart position +/- 2.4 or steps >T (given by gym)
 def run_trajectory():
     observation = env.reset()
-    env.seed(1995)
+    env.seed(1)
     transitions = []
     for t in range(T):
         env.render()
 
         # Choose best suitable action based on current observation using gaussian distribution
-        action = choose_action(observation)
+        p1, p2 = choose_action(observation)
+        action = np.random.choice([1, 0], p=[p1, p2])
 
         old_observation = observation
 
         # Execute action to get next state
         observation, reward, done, info = env.step(action)
-        global Reward
+        global Reward, max_reward
         Reward += reward
         # Stop trajectory if termination condition is met
         if done:
             # print("Reward = ", np.sum(R[n, :]))
-            if t < 199:
-                transitions.append((old_observation, action, -10))
+            if t < max_reward:
+                transitions.append((old_observation, action, -30))
             else:
+                max_reward = Reward
                 transitions.append((old_observation, action, reward))
             print("Trial finished after {} timesteps.".format(t + 1))
             break
@@ -173,7 +173,7 @@ def train_algorithm():
         else:
             optimal_counter = 0
 
-        if optimal_counter == 5:
+        if optimal_counter == 10:
             print("Assuming optimal parameters!")
             break;
 
@@ -183,8 +183,8 @@ def train_algorithm():
             print(W1)
             print(W2)
 
-    plt.plot(np.arange(K), reward_per_episode, 'r')
-    plt.plot(np.arange(K), mean_until_episode, 'b')
+    plt.plot(np.arange(len(reward_per_episode)), reward_per_episode, 'r')
+    plt.plot(np.arange(len(mean_until_episode)), mean_until_episode, 'b')
     plt.ylabel("Average Reward per Episode")
     plt.xlabel("Episodes")
     plt.show()
@@ -201,7 +201,11 @@ def run_benchmark():
         observation = env.reset()
         for t in range(T):
             env.render()
-            action = choose_action(observation)
+            p1, p2 = choose_action(observation)
+            if p1 >= p2:
+                action = 1
+            else:
+                action = 0
             observation, reward, done, info = env.step(action)
             total_rewards[i_episode] += reward
             if done:
