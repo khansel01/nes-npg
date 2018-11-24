980475b802d3dac8a0527ddcb96a085fea2ee46a
unknown <kay.hansel@hotmail.de>
2018-11-24 21:25:14 +0100

    add Softmax_policy.py as class

--- Softmax_policy.py
+++ Softmax_policy.py
@@ -1,113 +1,24 @@
-import argparse
-import gym
 import numpy as np
 
-import torch as tr
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.autograd as agrad
-from torch.distributions import Categorical
-from torch.distributions import Normal
+#######################################
+# Softmax Policy
+#######################################
 
 
-class Policy(nn.Module):
-    def __init__(self, obs_dim, act_dim):
-        super(Policy, self).__init__()
-        self.affine1 = nn.Linear(obs_dim, act_dim)
-        self.saved_log_probs = []
-        self.rewards = []
-        self.advantage = []
+class SoftmaxPolicy:
 
-    def get_action(self, observation):
-        obs = tr.from_numpy(observation).float().unsqueeze(0)
-        probs = self(obs)  # call forward function and get the probabilities for each action
-        m = Categorical(probs)  # create a categorical distribution of "probs"
-        action = m.sample()  # choose an action based on the distribution "m"
-        self.saved_log_probs.append(m.log_prob(action))
-        return action.item()  # get the single value as python number if tensor contains only single value
+    # Returns array of shape (1, n_actions) containing the probabilities
+    # of each action.
+    def get_action_prob(self, state, w):
+        # state.shape = (1,n) // w.shape = (n, n_actions)
+        x = state@w
+        x = np.exp(x)
+        return x/np.sum(x)
 
-    def forward(self, x):
-        action_scores = self.affine1(x)
-        return F.softmax(action_scores, dim=1)
+    # Returns the Jacobian matrix of the policy with respect to
+    # the parameters w.
+    def get_p_grad(self, state, w):
+        prob = self.get_action_prob(state, w)
+        prob = prob.reshape(-1, 1)
+        return np.diagflat(prob) - prob@prob.T
 
-    def clear(self):
-        del self.rewards[:]
-        del self.advantage[:]
-        return
-
-
-# # Choose an action based on higher probability using policy function
-# # TODO: Implement for arbitrary amount of actions
-# def choose_action(observation):
-#     a1 = softmax_policy(observation, 1.0, W1)
-#     a2 = softmax_policy(observation, 0.0, W2)
-#     # print("Exp: ", a1, a2)
-#     p1 = a1 / (a1+a2)
-#     p2 = a2 / (a1+a2)
-#     probs = np.matrix([p1, p2])
-#     probs = tr.from_numpy(probs)
-#     m = Categorical(probs)
-#     action = m.sample()
-#     return action.item()
-#     # print("Prob: ", p1, p2)
-#     if p1 >= p2:
-#         return 1
-#     else:
-#         return 0
-
-# def softmax_policy(observation, action, W):
-#     obs = np.zeros(len(observation))
-#     W = np.append(W, 1)
-#     for i, ele in enumerate(observation):
-#         obs[i] = ele
-#     phi = np.append(obs, action)
-#     return np.exp(np.dot(phi, np.transpose(W)))
-
-def main():
-    # Cart Pole Simulation
-    # --------------------------------
-    # Observations: 1. Cart Position   2. Cart Velocity    3. Pole Angle   4. Pole Velocity at Tip
-    # Actions: 1. Left  2. Right
-    env = gym.make('CartPole-v0')
-    env.seed(1995)
-
-    # Define Parameters [W, b, sigma]
-    # --------------------------------
-    # W consists of 4 weights - one for each observation
-    # delta is the normalized step size of the parameter update
-    W1 = np.zeros((1, 4)) * 0.0
-    W2 = np.zeros((1, 4)) * 0.0
-    delta = 0.05
-
-    # Define training setup
-    # --------------------------------
-    # gamma is the discount factor
-    # T is the max number of steps in a single run of the simulation
-    # K is the number of iterations for training the algorithm
-    T = 200
-    K = 1000
-    lambda_ = 0.95
-    gamma_ = 0.98
-    Reward = 0
-
-
-    observation = env.reset()
-    env.seed(1995)
-    env.render()
-    observation1, reward, done, _ = env.step(1)
-    policy = Policy(4, 2)
-    for i in range(2):
-        print("ob: ", np.transpose(observation1))
-        action = policy.get_action(observation1)
-        for p in policy.parameters():
-            print(i, "parameter: ", p)
-            t = p[1]
-            print("t:", t.requires_grad)
-            print(":")
-        env.render()
-        observation, reward, done, _ = env.step(action)
-    env.close()
-
-if __name__ == '__main__':
-    main()
