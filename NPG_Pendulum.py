6ddf4524d0011bed89cda1ba603a4c7fe221ec46
khansel01 <kay.hansel@hotmail.de>
2019-01-25 22:24:41 +0100

    current state

---
+++ NPG_Pendulum.py
@@ -0,0 +1,46 @@
+import torch as tr
+import numpy as np
+from Agent import Agent
+from NPG import NPG
+from models.NN_GaussianPolicy import Policy
+from utilities.Environment import Environment
+from models.Baseline import Baseline
+from utilities.Normalizer import Normalizer
+
+#######################################
+# Environment
+#######################################
+
+""" set seed """
+np.random.seed(0)
+tr.manual_seed(0)
+
+""" define the environment """
+gym_env = 'Pendulum-v0'
+print("===================== Start {} =====================".format(gym_env))
+env = Environment(gym_env)
+
+""" create policy """
+policy = Policy(env, hidden_dim=(8, 8), log_std=0)
+
+""" create baseline """
+baseline = Baseline(env, hidden_dim=(8, 8))
+
+""" create Normalizer to scale the states/observations """
+normalizer = Normalizer(env)
+
+""" create NPG-algorithm """
+algorithm = NPG(0.0025)
+
+""" create agent """
+agent = Agent(env, policy, algorithm, baseline, render=True)
+
+""" train the policy """
+agent.train_policy(100, 100, normalizer=normalizer)
+
+print("====================== DO Benchmark ======================")
+""" check the results """
+#   TODO benchmark has a bug
+agent.benchmark_test(episodes=2, render=True)
+
+
---
+++ NPG__Qube.py
@@ -0,0 +1,46 @@
+import torch as tr
+import numpy as np
+from Agent import Agent
+from NPG import NPG
+from models.NN_GaussianPolicy import Policy
+from utilities.Environment import Environment
+from models.Baseline import Baseline
+from utilities.Normalizer import Normalizer
+
+#######################################
+# Environment
+#######################################
+
+""" set seed """
+np.random.seed(0)
+tr.manual_seed(0)
+
+""" define the environment """
+gym_env = 'Qube-v0'
+print("===================== Start {} =====================".format(gym_env))
+env = Environment(gym_env)
+
+""" create policy """
+policy = Policy(env, hidden_dim=(32, 32), log_std=0)
+
+""" create baseline """
+baseline = Baseline(env, hidden_dim=(32, 32), epochs=10)
+
+""" create Normalizer to scale the states/observations """
+normalizer = Normalizer(env)
+
+""" create NPG-algorithm """
+algorithm = NPG(0.001)
+
+""" create agent """
+agent = Agent(env, policy, algorithm, baseline, _gamma=0.99, render=True)
+
+""" train the policy """
+agent.train_policy(1000, 20, normalizer=normalizer)
+
+print("====================== DO Benchmark ======================")
+""" check the results """
+#   TODO benchmark has a bug
+agent.benchmark_test(episodes=2, render=True)
+
+
