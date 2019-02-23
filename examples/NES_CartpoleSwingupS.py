c74c2127d83bc02132305f74be6e4352f8959124
VanillaWhey <cedric.derstroff@stud.tu-darmstadt.de>
2019-02-23 12:17:28 +0100

    NES can continue tzraning on loaded policies

--- examples/NES_CartpoleSwingupS.py
+++ examples/NES_CartpoleSwingupS.py
@@ -11,35 +11,36 @@ import pickle
 
 """ define the environment """
 gym_env = 'CartpoleSwingShort-v0'
-env = Environment(gym_env, horizon=3000, clip=5)
+env = Environment(gym_env, clip=3)
 
 print("================== Start {} ==================".format(gym_env))
 
 """ load pretrained data """
-# path = "{}_clipped_nes.p".format(gym_env)
-# pickle_in = open(path, "rb")
-# policy = pickle.load(pickle_in)
+
+path = "{}_3clipped100_nes.p".format(gym_env)
+pickle_in = open(path, "rb")
+policy = pickle.load(pickle_in)
 
 """ create policy """
-policy = Policy(env, hidden_dim=(10,))
-
-""" create NES-algorithm """
+# policy = Policy(env, hidden_dim=(10,))
+#
+# """ create NES-algorithm """
 algorithm = NES(policy.length, sigma_init=1.0)
 
 """ create agent """
 agent = Agent(env, policy, algorithm)
 
 """ train the policy """
-agent.train_policy(episodes=800, n_roll_outs=1)
+agent.train_policy(episodes=10, n_roll_outs=5)
 
 """ check the results """
 Helper.run_benchmark(policy, env)
 
 """ render one episode"""
-Helper.render(policy, env, step_size=1)
+Helper.render(policy, env, step_size=5)
 
 """ Save trained data """
-path = "{}_clipped_nes_short_horizon.p".format(gym_env)
+path = "{}_3clipped.2_nes.p".format(gym_env)
 pickle_out = open(path, "wb")
 pickle.dump(policy, pickle_out)
 pickle_out.close()
