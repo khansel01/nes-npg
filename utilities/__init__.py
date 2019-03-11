c971b82efc846281ebc6d6d2a03c80c8417f7c92
Unknown <janosch_moos@online.de>
2019-03-11 20:26:08 +0100

    FINAL

---
+++ utilities/__init__.py
@@ -0,0 +1,11 @@
+from utilities.conjugate_gradient import conjugate_gradient
+
+from utilities.environment import Environment
+
+from utilities.estimations import estimate_value
+from utilities.estimations import estimate_advantage
+from utilities.estimations import estimate_fitness
+
+from utilities.logger import Logger
+
+from utilities.normalizer import Normalizer
